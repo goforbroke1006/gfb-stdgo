18d28ce55f7ada61c252714f32ec58276a116d98
goforbroke <go.for.broke1006@gmail.com>
2019-02-11 20:48:43 +0300

    Fix

--- test/scripts/bootstrap.sh
+++ test/scripts/bootstrap.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get install -y libgtest-dev
+cd /usr/src/gtest
+sudo cmake CMakeLists.txt
+sudo make
+sudo cp *.a /usr/lib # copy or symlink libgtest.a and libgtest_main.a to your /usr/lib folder
