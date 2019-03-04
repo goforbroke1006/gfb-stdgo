6d0c96dc35a640afb29529b8972a069ba9c0c12c
Sergey <go.for.broke1006@gmail.com>
2019-03-04 12:23:30 +0300

    Create http.h

---
+++ include/net/http.h
@@ -0,0 +1,29 @@
+#ifndef GFB_STDGO_NET_HTTP_H
+#define GFB_STDGO_NET_HTTP_H
+
+#include <string>
+#include <map>
+
+namespace gfb {
+    namespace stdgo {
+        namespace net {
+            namespace http {
+                class Transport {};
+                class Client {};
+                class Server {
+                public:
+                    Server(const std::string &addr, void *handler, 
+                        const ssize_t &readTimeout, const ssize_t &writeTimeout) {}
+                };
+                
+                class Response {};
+                
+                Response Get(const std::string &url);
+                Response Post(const std::string &url, const std::string &contentType, const std::string &body);
+                Response PostForm(const std::string &url, const std::map<std::string, std::string> &data);
+            }
+        }
+    }
+}
+
+#endif //GFB_STDGO_NET_HTTP_H
