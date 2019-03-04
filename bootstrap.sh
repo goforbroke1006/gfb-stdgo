8032bd9b13356536f7d4bad7dec13a845aaf484e
Sergey Cherkesov <s.cherkesov@pbt.com.mt>
2019-03-04 18:55:35 +0300

    Try to prepare build under Win

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,22 +11,25 @@ set -e
 unameOut="$(uname -s)"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+
+if [[ ${unameOut} =~ Linux* ]]; then
+    sudo apt-get install -y unzip libssl-dev libcurl4-gnutls-dev
+fi
+
+if [[ ${unameOut} =~ CYGWIN* ]] || [[ ${unameOut} =~ MINGW* ]]; then
+    wget https://curl.haxx.se/download/curl-7.20.0.zip
+    unzip curl-7.20.0.zip -d ~/
+    cd ~/curl-7.20.0
+    ./configure
+    make
+    make install
+fi
+
+
 mkdir -p ${SCRIPT_DIR}/vendor/
 cd ${SCRIPT_DIR}/vendor
 
-if [[ ! -d curl ]]; then
-    git clone --branch curl-7_64_0 --depth 1 https://github.com/curl/curl.git
-    cd curl
-    mkdir -p build && cd build
-    if [[ ${unameOut} =~ CYGWIN* ]] || [[ ${unameOut} =~ MINGW* ]]; then
-        cmake ..
-        cmake --build . --target install
-    fi
-    if [[ ${unameOut} =~ Linux* ]]; then
-        sudo apt-get install -y libssl-dev
-        cmake ..
-        sudo cmake --build . --target install
-    fi
-fi
+# ---------------------
+# ---------------------
 
 cd ${SCRIPT_DIR}/vendor
