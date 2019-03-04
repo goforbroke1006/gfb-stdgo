df6d4992efbc1fa46730852248d80cd00ffbe453
GoForBroke <go.for.broke1006@gmail.com>
2019-03-04 16:36:02 +0300

    Try to fix win32 build

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,25 @@ set -e
 #mkdir -p ${PROJECT_ROOT}/vendor
 #curl https://raw.githubusercontent.com/HowardHinnant/date/master/include/date/date.h > ${PROJECT_ROOT}/vendor/date.h
 
+unameOut="$(uname -s)"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+mkdir -p ${SCRIPT_DIR}/vendor/
+cd ${SCRIPT_DIR}/vendor
 
-sudo apt-get update
-sudo apt-get install -y libcurl4-gnutls-dev
+if [[ ! -d curl ]]; then
+    git clone --branch curl-7_64_0 --depth 1 https://github.com/curl/curl.git
+    cd curl
+    mkdir -p build && cd build
+    if [[ ${unameOut} =~ CYGWIN* ]] || [[ ${unameOut} =~ MINGW* ]]; then
+        cmake ..
+        cmake --build . --target install
+    fi
+    if [[ ${unameOut} =~ Linux* ]]; then
+        sudo apt-get install -y libssl-dev
+        cmake ..
+        sudo cmake --build . --target install
+    fi
+fi
 
+cd ${SCRIPT_DIR}/vendor
